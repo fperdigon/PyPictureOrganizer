4d20f817e11c1b7a90f2e9e475baca3e77aed61c
fperdigon <fperdigon88@gmail.com>
2022-08-23 14:07:35 -0400

    Adding main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+# This software organize pictures from unorganized folders to organized ones.
+# Useful when you organize your pictures from your phone and then your mate
+# or friend gives you a bunch of pictures from past date
+
+
+
+un_org_folder = ''
+org_folder = ''
