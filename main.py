11090ae82557fd4f3ffa9d031fa7c7a2119830a9
fperdigon <fperdigon88@gmail.com>
2022-08-23 22:06:32 -0400

    Fixing a typo in the file name

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 # Useful when you organize your pictures from your phone and then your mate
 # or friend gives you a bunch of pictures from past date
 
-import pypicureorganizer_core
+import pypictureorganizer_core
 
 un_org_folder = '/media/fco/DATA/Cloud/Fotos_Org/'
 org_folder = '/media/fco/DATA/Cloud/Fotos/'
 
-pypicureorganizer_core.organize(un_org_folder, org_folder, extension='.jpg')
-pypicureorganizer_core.organize(un_org_folder, org_folder, extension='.mkv')
-pypicureorganizer_core.organize(un_org_folder, org_folder, extension='.mp4')
+pypictureorganizer_core.organize(un_org_folder, org_folder, extension='.jpg')
+pypictureorganizer_core.organize(un_org_folder, org_folder, extension='.mkv')
+pypictureorganizer_core.organize(un_org_folder, org_folder, extension='.mp4')
